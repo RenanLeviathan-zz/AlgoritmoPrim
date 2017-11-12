233d78f6b3b596443cbd687c808d6ecff39c14a8
RenanLeviathan <renan.leviathan@gmail.com>
2017-11-12 10:24:41 -0300

    #2.0

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 @note: Implementação do algoritmo de Prim
 '''
 from random import choice
+from plot import Plot
 grafo={
   '0':{'2':5,'3':8},
   '1':{'2':16,'4':30,'6':26},
@@ -13,6 +14,16 @@ grafo={
   '5':{'3':18,'4':12,'6':14},
   '6':{'1':26,'4':14,'5':4}
 }
+#coordenadas dos vértices
+pos={
+  '0':(50,100),
+  '1':(200,50),
+  '2':(100,50),
+  '3':(100,150),
+  '4':(150,100),
+  '5':(200,150),
+  '6':(250,100)
+  }
 
 global T#variável global de escolha do vértice
 T=[]
@@ -40,3 +51,5 @@ while len(T)!=len(lst):
   V=[x for x in V if x!=q]
   Tmin.append((p,q))
 print(Tmin)
+pl=Plot(Tmin,pos)
+pl.plot("Árvore geradora mínima")
